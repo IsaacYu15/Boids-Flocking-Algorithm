b4ffba7943d957f7a4b7744059f61101258ef49d
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-12-20 11:51:41 -0500

    feat: preventing collision between boids

--- Flocking Algorithm/Assets/Boids/BoidMovement.cs
+++ Flocking Algorithm/Assets/Boids/BoidMovement.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class BoidMovement : MonoBehaviour
 {
+    public bool subjectOfStudy;
+
     public LayerMask boundaries;
+    public LayerMask boids;
 
     public float bounds;
     public float speed = 0.5f;
@@ -26,14 +29,15 @@ public class BoidMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + velocity * speed;
         transform.rotation = Quaternion.LookRotation(velocity);
+        transform.position = transform.position + transform.forward * speed;
 
         if (Mathf.Abs(transform.position.x) > bounds || Mathf.Abs(transform.position.z) > bounds )
         {
             respawnBoid();
         }
-        
+
+        avoidCollisions();
     }
 
     void respawnBoid ()
@@ -44,5 +48,25 @@ public class BoidMovement : MonoBehaviour
             transform.position = hit.transform.position;
         }
 
+    }
+
+    //NEEDS WORK
+    void avoidCollisions ()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 5);
+        foreach(var hitCollider in hitColliders)
+        {
+            if (hitCollider.transform.gameObject.layer == Mathf.RoundToInt(Mathf.Log(boids.value,2))
+                && hitCollider.transform.gameObject != this.gameObject)
+            {
+                float angle = Vector3.Angle(transform.forward, hitCollider.gameObject.transform.position - transform.position);
+                if (Mathf.Abs(angle) < 120)
+                {
+                    //Debug.DrawLine(hitCollider.transform.position, transform.position, Color.black);
+                    velocity = (velocity - hitCollider.gameObject.GetComponent<BoidMovement>().velocity).normalized;
+                }
+
+            }
+        }
     }
 }
